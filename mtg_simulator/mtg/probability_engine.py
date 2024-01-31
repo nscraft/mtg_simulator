c498d3613bd74245c9bd967f680aec332455defa
nscraft <104389847+nscraft@users.noreply.github.com>
2024-01-31 16:51:13 -0700

    Update probability_engine.py
    updating descriptions

--- mtg_simulator/mtg/probability_engine.py
+++ mtg_simulator/mtg/probability_engine.py
@@ -1,21 +1,24 @@
 from math import comb
 
+
 # Population as N
 # Num Successes in Population as K
 # Sample as n
 # Num Successes in Sample as k
 
-
+# probability mass function (PMF)
 def hypergeom_pmf(N, K, n, k):
     """Calculate the probability of drawing exactly k successes."""
     return (comb(K, k) * comb(N - K, n - k)) / comb(N, n)
 
 
+# cumulative distribution function (CDF)
 def hypergeom_cdf(N, K, n, k):
     """Calculate the probability of drawing k or less successes."""
     return sum(hypergeom_pmf(N, K, n, ki) for ki in range(k + 1))
 
 
+# survival function (SF)
 def hypergeom_sf(N, K, n, k):
     """Calculate the probability of drawing more than k successes."""
     return 1 - hypergeom_cdf(N, K, n, k)
@@ -32,8 +35,8 @@ def P_at_exactly_k(N, K, n, k):
 
 
 # Cumulative Probability :P(<= x)
-def P_at_most_k(self):
-    return self.hypergeom_cdf(self.k)
+def P_at_most_k(N, K, n, k):
+    return hypergeom_cdf(N, K, n, k)
 
 
 # Cumulative Probability :P(> x)
