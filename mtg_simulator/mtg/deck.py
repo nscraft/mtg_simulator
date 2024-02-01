448b83123ca6b3a6a4a3ef615691a3de5c53fb95
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-01 15:16:31 -0700

    Update deck.py

--- mtg_simulator/mtg/deck.py
+++ mtg_simulator/mtg/deck.py
@@ -1,10 +1,4 @@
-from data.deck_loader import load_deck_excel, write_deck, write_deck_withpartners
 from mtg.probability_engine import P_more_than_k, P_at_exactly_k
-
-# initialize decks either from Excel files or random gen dfs
-deck_df1 = write_deck()
-deck_df2 = write_deck_withpartners()
-deck_excel1 = load_deck_excel('rand_deck_1.xlsx')
 
 
 def deck_size(self):
@@ -69,28 +63,3 @@ def four_land_opener(self):
     cards_drawn = 7
     min_lands = 4
     return P_more_than_k(N=deck_library(self), K=num_land(self), n=cards_drawn, k=min_lands)
-
-
-def deck_reporter(deck):  # will need to add a deck name argument later
-    report = (
-        f"Deck Stats: ~deckname~\n"
-        f"{num_commander(deck)} commander(s) with {deck_library(deck)} cards in library.\n"
-        f"Total of {num_land(deck)} lands.\n"
-        f"Average mana cost of non-land cards is {avg_mana_cost(deck)}.\n"
-        f"Total number of ramp cards is {num_ramp(deck)} and total number of key cards is {num_key(deck)}.\n"
-        f"The average ramp value is {avg_ramp_value(deck)} and the average key value is {avg_key_value(deck)}.\n"
-        f"There are {deck_size(deck)-(num_land(deck)+num_ramp(deck)+num_key(deck)+num_commander(deck))} other (aka: "
-        f"noncommander, nonland, nonramp, nonkey) cards.\n"
-        f"Probabilities: ~deckname~\n"
-        f"Probability of drawing 2-3 land on turn 1?\n"
-        f"{round(four_land_opener(deck) * 100, 1)}% chance of starting with 4 or more lands.\n"
-        f"{round(zero_land_opener(deck) * 100, 1)}% chance of starting with zero land.\n"
-        f"Probability of drawing 5 or more unplayable cards on turn 1?\n"
-        f"Probability of drawing 5 or more unplayable cards by turn 3?\n"
-        f"Probability of drawing playable ramp by turn 2?\n"
-        f"Probability of drawing playable key card by turn 4?\n"
-    )
-    return report
-
-
-print(deck_reporter(deck_df1))
