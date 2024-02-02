7ab20b0420a2d18fba470aee7cfec42ab27fb724
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-02 16:19:58 -0700

    Update main.py
    added output for goldfish game

--- mtg_simulator/main.py
+++ mtg_simulator/main.py
@@ -1,15 +1,13 @@
 from data.deck_loader import DeckExcelMethod, write_deck
 from mtg.deck import DeckMetrics, OpeningHandProbabilities, deck_reporter
-from mtg.game import pick_card, island
+from mtg.game import pick_card, island, GoldfishGame
 
 # initialize decks either from Excel files or random gen dfs
 deck1_name = "Programed RandDeck"
 deck1_df = write_deck()
-deck2 = DeckExcelMethod('rand_deck_1.xlsx')
+deck2 = DeckExcelMethod('deck_sample.xlsx')
 deck2_name = DeckExcelMethod.deck_name(deck2)
 deck2_df = DeckExcelMethod.load_deck_excel(deck2)
-
-
 
 # Initialize DeckMetrics and OpeningHandProbabilities instances for each deck
 deck_metrics1 = DeckMetrics(deck1_df)
@@ -21,13 +19,25 @@ opening_hand_probs2 = OpeningHandProbabilities(deck1_df)
 print(deck_reporter(deck1_name, deck_metrics1, opening_hand_probs1))
 print(deck_reporter(deck2_name, deck_metrics2, opening_hand_probs2))
 
-# Pick cards for opening hands
+# Use pick_card method
 opening_hand1 = pick_card(deck_metrics1.deck_library()['card_slot'], 7)
-opening_hand2 = pick_card(deck_metrics2.deck_library()['card_slot'], 7)
-
-# Print opening hands and land counts
 print(f"{deck1_name} opening hand: {opening_hand1}")
 print(f"{island(deck1_df, opening_hand1)} Lands in deck1 opening hand")
 
-print(f"{deck2_name} opening hand: {opening_hand2}")
-print(f"{island(deck2_df, opening_hand2)} Lands in deck2 opening hand")
+# Run a goldfish game
+game = GoldfishGame(deck1_df)
+print("\n START GAME: GOLDFISH")
+goldfish_open = game.draw_card(num_card=7)  # Draw 7 card opener
+print(f"Opening hand: {game.hand}")
+print(f"Cards in Library: {len(game.library)}")
+print(f"Cards in Graveyard: {len(game.graveyard)}")
+selected_cards_to_discard = {25, 50, 75}  # Replace with actual card slots to discard
+#  game.discard_cards(selected_cards_to_discard)
+print(f"Updated hand: {game.hand}")
+print(f"Cards in Updated Library: {len(game.library)}")
+print(f"Cards in Updated Graveyard: {len(game.graveyard)}")
+goldfish_draw_more = 7
+additional_cards_drawn = game.draw_card(goldfish_draw_more)
+print(f"Updated hand: {game.hand}")
+print(f"Cards in Updated Library: {len(game.library)}")
+print(f"Cards in Updated Graveyard: {len(game.graveyard)}")
