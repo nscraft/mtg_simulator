d476a2139630f0b6b2823ff0e3c31d5fbdbdcc12
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-02 10:43:37 -0700

    Update test_game.py

--- mtg_simulator/test/test_game.py
+++ mtg_simulator/test/test_game.py
@@ -1,5 +1,6 @@
 import unittest
-from mtg.game import pick_card
+import pandas as pd
+from mtg.game import pick_card, island
 
 
 class TestDeck(unittest.TestCase):
@@ -14,6 +15,19 @@ class TestDeck(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertEqual(count, self.cards)
         self.assertTrue(all(card in self.pool for card in result))
+
+
+class TestCardsInHand(unittest.TestCase):
+
+    def setUp(self):
+        self.deck_df = pd.DataFrame(
+            {'card_slot': [1, 2, 3, 4, 5],
+             'island': [0, 0, 1, 1, 0]})
+        self.set = {2, 3}
+
+    def test_island(self):
+        result = island(self.deck_df, self.set)
+        self.assertIsNotNone(result)
 
 
 if __name__ == '__main__':
