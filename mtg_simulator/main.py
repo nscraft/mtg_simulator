38f8f1d27fc4b6b62190aa4cd0c15c200566bb1c
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-02 12:21:23 -0700

    Update main.py
    added feature for deck name

--- mtg_simulator/main.py
+++ mtg_simulator/main.py
@@ -1,28 +1,33 @@
-from data.deck_loader import load_deck_excel, write_deck
+from data.deck_loader import DeckExcelMethod, write_deck
 from mtg.deck import DeckMetrics, OpeningHandProbabilities, deck_reporter
 from mtg.game import pick_card, island
 
 # initialize decks either from Excel files or random gen dfs
-deck_df1 = write_deck()
-deck_df2 = load_deck_excel('rand_deck_1.xlsx')
+deck1_name = "Programed RandDeck"
+deck1_df = write_deck()
+deck2 = DeckExcelMethod('rand_deck_1.xlsx')
+deck2_name = DeckExcelMethod.deck_name(deck2)
+deck2_df = DeckExcelMethod.load_deck_excel(deck2)
+
+
 
 # Initialize DeckMetrics and OpeningHandProbabilities instances for each deck
-deck_metrics1 = DeckMetrics(deck_df1)
-deck_metrics2 = DeckMetrics(deck_df2)
-opening_hand_probs1 = OpeningHandProbabilities(deck_df1)
-opening_hand_probs2 = OpeningHandProbabilities(deck_df2)
+deck_metrics1 = DeckMetrics(deck1_df)
+deck_metrics2 = DeckMetrics(deck2_df)
+opening_hand_probs1 = OpeningHandProbabilities(deck1_df)
+opening_hand_probs2 = OpeningHandProbabilities(deck1_df)
 
 # Print deck stats and probabilities
-print(deck_reporter(deck_metrics1, opening_hand_probs1))
-print(deck_reporter(deck_metrics2, opening_hand_probs2))
+print(deck_reporter(deck1_name, deck_metrics1, opening_hand_probs1))
+print(deck_reporter(deck2_name, deck_metrics2, opening_hand_probs2))
 
 # Pick cards for opening hands
 opening_hand1 = pick_card(deck_metrics1.deck_library()['card_slot'], 7)
 opening_hand2 = pick_card(deck_metrics2.deck_library()['card_slot'], 7)
 
 # Print opening hands and land counts
-print(f"deck1 opening hand: {opening_hand1}")
-print(f"{island(deck_df1, opening_hand1)} Lands in deck1 opening hand")
+print(f"{deck1_name} opening hand: {opening_hand1}")
+print(f"{island(deck1_df, opening_hand1)} Lands in deck1 opening hand")
 
-print(f"deck2 opening hand: {opening_hand2}")
-print(f"{island(deck_df2, opening_hand2)} Lands in deck2 opening hand")
+print(f"{deck2_name} opening hand: {opening_hand2}")
+print(f"{island(deck2_df, opening_hand2)} Lands in deck2 opening hand")
