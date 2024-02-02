cd025a795434528f026933b79d1657caaf3c7adf
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-02 10:35:32 -0700

    Update main.py
    can now count lands in opening hand

--- mtg_simulator/main.py
+++ mtg_simulator/main.py
@@ -1,5 +1,6 @@
 from data.deck_loader import load_deck_excel, write_deck
 from mtg.deck import deck_reporter
+from mtg.game import pick_card, island
 
 # initialize decks either from Excel files or random gen dfs
 deck_df1 = write_deck()
@@ -9,3 +10,9 @@ deck_df2 = load_deck_excel('rand_deck_1.xlsx')
 
 print(deck_reporter(deck_df1))
 print(deck_reporter(deck_df2))
+
+# now picking (aka drawing) cards from a deck and evaluating results
+
+opening_hand = pick_card(deck_df1['card_slot'], 7)
+print(f"deck1 opening hand {opening_hand}")
+print(f"{island(deck_df1, opening_hand)} Lands in opening hand")
