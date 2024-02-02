348cd8431b0836f3de7df22658bc0dbeb4bb7e2d
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-02 16:20:31 -0700

    added goldfishgame
    added goldfishgame

--- mtg_simulator/mtg/game.py
+++ mtg_simulator/mtg/game.py
@@ -5,6 +5,7 @@
 import random
 
 
+# this class is deprecated
 class UniqueRandomGenerator:
     def __init__(self, start, end):
         self.start = start
@@ -37,3 +38,33 @@ def island(deck, cards):
     island_inHand = lands_inDeck['card_slot'].isin(cards)
     land_count = island_inHand.sum()
     return land_count
+
+
+class GoldfishGame:
+    def __init__(self, deck_df):
+        self.library = set(deck_df['card_slot'])
+        self.hand = set()
+        self.graveyard = set()
+
+    def draw_card(self, num_card):
+        if num_card > len(self.library):
+            raise ValueError("Not enough cards in the library to draw.")
+        drawn_cards = set(random.sample(self.library, num_card))
+        self.library -= drawn_cards
+        self.hand.update(drawn_cards)
+        return drawn_cards
+
+    def discard_cards(self, cards_to_discard):
+        if not cards_to_discard.issubset(self.hand):
+            raise ValueError("Cannot discard cards that are not in hand.")
+        self.hand -= cards_to_discard
+        self.graveyard.update(cards_to_discard)
+
+    def get_cards_inhand(self):
+        return self.hand
+
+    def get_cards_inlibrary(self):
+        return self.library
+
+    def get_cards_ingrave(self):
+        return self.graveyard
