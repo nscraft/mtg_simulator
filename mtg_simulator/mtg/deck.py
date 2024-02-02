bd5d4d7f6c14f215b0f7a5276992faac7d0f8c01
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-02 11:18:41 -0700

    Update deck.py

--- mtg_simulator/mtg/deck.py
+++ mtg_simulator/mtg/deck.py
@@ -1,121 +1,113 @@
 from mtg.probability_engine import P_more_than_k, P_at_exactly_k
 
+class DeckMetrics:
+    def __init__(self, deck):
+        self.deck = deck
 
-def deck_size(self):
-    card_count = max(self.card_slot)
-    return card_count
+    def deck_size(self):
+        return max(self.deck.card_slot)
 
+    def deck_library(self):
+        return len(self.deck[self.deck.iscommander == 0])
 
-def deck_library(self):
-    library_df = self[self.iscommander == 0]
-    card_count = len(library_df)
-    return card_count
+    def num_commander(self):
+        return len(self.deck[self.deck.iscommander == 1])
 
+    def num_land(self):
+        return len(self.deck[self.deck.island == 1])
 
-def num_commander(self):
-    commander_df = self[self.iscommander == 1]
-    return len(commander_df)
+    def num_ramp(self):
+        return len(self.deck[self.deck.isramp == 1])
 
+    def num_key(self):
+        return len(self.deck[self.deck.iskey == 1])
 
-def num_land(self):
-    land_df = self[self.island == 1]
-    return len(land_df)
+    def num_rampkey(self):
+        return len(self.deck[self.deck.isramp == 1 & self.deck.iskey == 1])
 
+    def num_other(self):
+        other_df = self.deck[
+            (self.deck.iscommander == 0) &
+            (self.deck.island == 0) &
+            (self.deck.isramp == 0) &
+            (self.deck.iskey == 0)
+            ]
+        other_count = len(other_df)
+        return other_count
 
-def avg_mana_cost(self):
-    nonland_df = self[self.island == 0]
-    avg = nonland_df['mana_cost'].mean()
-    round_avg = round(avg, 2)
-    return round_avg
+    def avg_mana_cost(self):
+        avg = self.deck[self.deck.island == 0].mean()
+        round_avg = round(avg, 2)
+        return round_avg
 
+    def avg_ramp_value(self):
+        ramp_df = self.deck[self.deck.isramp == 1]
+        avg = ramp_df['ramp_value'].mean()
+        round_avg = round(avg, 2)
+        return round_avg
 
-def num_ramp(self):
-    ramp_df = self[self.isramp == 1]
-    return len(ramp_df)
-
-
-def num_ramp_key(self):
-    filter_df = self[
-        (self.isramp == 1) & (self.iskey == 1)
-        ]
-    count = len(filter_df)
-    return count
-
-
-def num_other(self):
-    other_df = self[
-        (self.iscommander == 0) &
-        (self.island == 0) &
-        (self.isramp == 0) &
-        (self.iskey == 0)
-        ]
-    other_count = len(other_df)
-    return other_count
-
-
-def avg_ramp_value(self):
-    ramp_df = self[self.isramp == 1]
-    avg = ramp_df['ramp_value'].mean()
-    round_avg = round(avg, 2)
-    return round_avg
+    def avg_key_value(self):
+        key_df = self.deck[self.deck.iskey == 1]
+        avg = key_df['key_value'].mean()
+        round_avg = round(avg, 2)
+        return round_avg
 
 
-def num_key(self):
-    key_df = self[self.iskey == 1]
-    return len(key_df)
+class OpeningHandProbabilities:
+    cards_drawn = 7
+
+    def __init(self, deck):
+        self.deck = deck
+        self.deck_metrics = DeckMetrics(deck)
+
+    def zero_land_opener(self):
+        hits = 0
+        probability = P_at_exactly_k(N=self.deck_metrics.deck_library(), K=self.deck_metrics.num_land(),
+                                     n=self.cards_drawn, k=hits)
+        return probability
+
+    def fourormore_land_opener(self):
+        hits = 4
+        probability = P_more_than_k(N=self.deck_metrics.deck_library(), K=self.deck_metrics.num_land(),
+                                    n=self.cards_drawn, k=hits)
+        return probability
+
+    def twothree_land_opener(self):
+        minhit = 2
+        maxhit = 3
+        minprobability = P_at_exactly_k(N=self.deck_metrics.deck_library(), K=self.deck_metrics.num_land(),
+                                     n=self.cards_drawn, k=minhit)
+        maxprobability = P_at_exactly_k(N=self.deck_metrics.deck_library(), K=self.deck_metrics.num_land(),
+                                        n=self.cards_drawn, k=maxhit)
+        return minprobability + maxprobability
+
+    def five_unplayable_opener(self):
+        filter_deck = self.deck[
+            (self.deck['island'] == 0) & (self.deck['mana_cost'] > 1)
+            ]
+        min_unplayable = 5
+        probability = P_more_than_k(N=self.deck_metrics.deck_library(), K=len(filter_deck), n=self.cards_drawn,
+                                    k=min_unplayable)
+        return probability
 
 
-def avg_key_value(self):
-    key_df = self[self.iskey == 1]
-    avg = key_df['key_value'].mean()
-    round_avg = round(avg, 2)
-    return round_avg
-
-
-def zero_land_opener(self):
-    cards_drawn = 8
-    desired_lands = 0
-    probability = P_at_exactly_k(N=deck_library(self), K=num_land(self), n=cards_drawn, k=desired_lands)
-    return probability
-
-
-def four_land_opener(self):
-    cards_drawn = 8
-    min_lands = 4
-    probability = P_more_than_k(N=deck_library(self), K=num_land(self), n=cards_drawn, k=min_lands)
-    return probability
-
-def twothree_land_opener(self):
-    cards_drawn = 8
-    probability = P_at_exactly_k(N=deck_library(self), K=num_land(self), n=cards_drawn, k=2) + P_at_exactly_k(N=deck_library(self), K=num_land(self), n=cards_drawn, k=3)
-    return probability
-
-def five_unplayable_opener(self):
-    cards_drawn = 8
-    filter_deck = self[
-        (self['island'] == 0) & (self['mana_cost'] > 1)
-        ]
-    min_unplayable = 5
-    probability = P_more_than_k(N=deck_library(self), K=len(filter_deck), n=cards_drawn, k=min_unplayable)
-    return probability
-
-
-def deck_reporter(deck):  # will need to add a deck name argument later
+def deck_reporter(deck_metrics, opening_hand_probs):  # will need to add a deck name argument later
     report = (
         f"DECKNAME\n"
         f"Deck Stats:\n"
-        f"{num_commander(deck)} commander(s) with {deck_library(deck)} cards in library.\n"
-        f"Total of {num_land(deck)} lands.\n"
-        f"Average mana cost of non-land cards is {avg_mana_cost(deck)}.\n"
-        f"Total number of ramp cards is {num_ramp(deck)} and total number of key cards is {num_key(deck)}.\n"
-        f"The average ramp value is {avg_ramp_value(deck)} and the average key value is {avg_key_value(deck)}.\n"
-        f"There are {num_other(deck)} other (aka: "
+        f"{deck_metrics.num_commander()} commander(s) with {deck_metrics.deck_library()} cards in library.\n"
+        f"Total of {deck_metrics.num_land()} lands.\n"
+        f"Average mana cost of non-land cards is {deck_metrics.avg_mana_cost()}.\n"
+        f"Total number of ramp cards is {deck_metrics.num_ramp()} and total number of key cards is {deck_metrics.num_key()}.\n"
+        f"The average ramp value is {deck_metrics.avg_ramp_value()} and the average key value is {deck_metrics.avg_key_value()}.\n"
+        f"There are {deck_metrics.num_other()} other (aka: "
         f"noncommander, nonland, nonramp, nonkey) cards.\n"
         f"Probabilities:\n"
-        f"{round(twothree_land_opener(deck) * 100, 1)}% chance of drawing 2-3 land on turn 1?\n"
-        f"{round(four_land_opener(deck) * 100, 1)}% chance of starting with 4 or more lands.\n"
-        f"{round(zero_land_opener(deck) * 100, 1)}% chance of starting with zero land.\n"
-        f"{round(five_unplayable_opener(deck) * 100, 1)}% chance of drawing 5 or more nonland cards that cost more than 1 mana on turn 1?\n"
+        f"{round(opening_hand_probs.twothree_land_opener() * 100, 1)}% chance of drawing 2-3 land on turn 1?\n"
+        f"{round(opening_hand_probs.fourormore_land_opener() * 100, 1)}% chance of starting with 4 or more lands.\n"
+        f"{round(opening_hand_probs.zero_land_opener() * 100, 1)}% chance of starting with zero land.\n"
+        f"{round(opening_hand_probs.five_unplayable_opener() * 100, 1)}% chance of drawing 5 or more nonland cards "
+        f"that cost more than 1 mana on turn 1?\n"
         f"Probability of drawing playable ramp by turn 2?\n"
         f"Probability of drawing playable key card by turn 4?\n"
     )
