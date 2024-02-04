19bd448e717939b207d4447958f7ebcd22a10516
nscraft <104389847+nscraft@users.noreply.github.com>
2024-02-04 08:25:29 -0700

    Update game.py
    modified godlfish library from set to list so that it can work with random.sample which requires the population passed to be a sequence

--- mtg_simulator/mtg/game.py
+++ mtg_simulator/mtg/game.py
@@ -42,15 +42,15 @@ def island(deck, cards):
 
 class GoldfishGame:
     def __init__(self, deck_df):
-        self.library = set(deck_df['card_slot'])
+        self.library = list(deck_df['card_slot'])
         self.hand = set()
         self.graveyard = set()
 
     def draw_cards(self, num_cards):
         if num_cards > len(self.library):
             raise ValueError("Not enough cards in the library to draw.")
-        drawn_cards = set(random.sample(self.library, num_cards))
-        self.library -= drawn_cards
+        drawn_cards = random.sample(population=self.library, k=num_cards)
+        self.library = [card for card in self.library if card not in drawn_cards]
         self.hand.update(drawn_cards)
         return drawn_cards
 
