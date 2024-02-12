4f3eda96e28e62b5e25503dedf7cfbefe6b8260e
Nick Craft <nscraft@gmail.com>
2024-02-11 20:03:01 -0700

    game wip

--- Project/Data/deck_loader.py
+++ Project/Data/deck_loader.py
@@ -22,7 +22,10 @@ def write_deck():
     deck_list['island'] = np.where(deck_list['iscommander'] == 1, 0, np.random.randint(0, 2, size=len(deck_list)))
     deck_list['mana_cost'] = np.where(deck_list['island'] == 1, 0, np.random.randint(0, 8, size=len(deck_list)))
     deck_list['isramp'] = np.where(deck_list['island'] == 1, 0, np.random.randint(0, 2, size=len(deck_list)))
-    deck_list['ramp_value'] = np.where(deck_list['isramp'] == 0, 0, np.random.randint(1, 5, size=len(deck_list)))
+    deck_list['mana_value'] = np.where(
+        deck_list['island'] == 1, 1,
+        np.where(deck_list['isramp'] == 1, np.random.randint(1, 5, size=len(deck_list)), 0)
+    )
     deck_list['isdraw'] = np.where(deck_list['island'] == 1, 0, np.random.randint(0, 2, size=len(deck_list)))
     deck_list['draw_value'] = np.where(deck_list['isdraw'] == 0, 0, np.random.randint(1, 8, size=len(deck_list)))
     deck_list['card_score'] = np.where(
