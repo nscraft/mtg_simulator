d5f35db6ddcff594ce932505ea06c364fc974e84
nick.craft <nscraft@gmail.com>
2024-02-12 09:42:12 -0700

    user input wip

--- Project/main.py
+++ Project/main.py
@@ -17,27 +17,3 @@ opening_hand_probs1 = OpeningHandProbabilities(deck1_df)
 
 # Print deck stats and probabilities
 print(deck_reporter(deck1_name, deck_metrics1, opening_hand_probs1))
-
-
-# Run a goldfish game
-game = GoldfishGame(deck1_df)
-print("\nSTART GAME: GOLDFISH")
-goldfish_open = game.draw_cards(num_cards=7)  # Draw 7 card opener
-print(f"Opening hand: {game.hand}")
-print(f"Cards in Library: {len(game.library)}")
-print(f"Cards in Graveyard: {len(game.graveyard)}")
-game.discard_hand()
-print("Hand discarded.")
-print(f"Updated hand: {game.hand}")
-print(f"Cards in Updated Library: {len(game.library)}")
-print(f"Cards in Updated Graveyard: {len(game.graveyard)}")
-try:
-    goldfish_draw_more = int(input("How many more cards would you like to draw?"))
-except ValueError:
-    print("Please enter a valid integer.")
-    goldfish_draw_more = 0
-print(f"{goldfish_draw_more} more cards drawn.")
-additional_cards_drawn = game.draw_cards(goldfish_draw_more)
-print(f"Updated hand: {game.hand}")
-print(f"Cards in Updated Library: {len(game.library)}")
-print(f"Cards in Updated Graveyard: {len(game.graveyard)}")
