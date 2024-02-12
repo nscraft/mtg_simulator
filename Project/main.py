631d04f9572bd8a179d8ae4f6ca5a093a214c8af
nick.craft <nscraft@gmail.com>
2024-02-12 10:44:57 -0700

    user input wip

--- Project/main.py
+++ Project/main.py
@@ -15,7 +15,29 @@ deck2_df = DeckExcelMethod.load_deck_excel(deck2)
 deck_metrics1 = DeckMetrics(deck1_df)
 opening_hand_probs1 = OpeningHandProbabilities(deck1_df)
 
-# Print deck stats and probabilities
-print(deck_reporter(deck1_name, deck_metrics1, opening_hand_probs1))
 
-start_goldfish_game(deck1_df)
+def print_menu():
+    print("\nMenu:")
+    print("1. Print deck report for deck 1")
+    print("2. Start Goldfish Game")
+    print("3. Exit")
+
+
+def main():
+    while True:
+        print_menu()
+        choice = input("Enter your choice (1-3): ")
+
+        if choice == '1':
+            print(deck_reporter(deck1_name, deck_metrics1, opening_hand_probs1))
+        elif choice == '2':
+            start_goldfish_game(deck1_df)
+        elif choice == '3':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please choose again.")
+
+
+if __name__ == "__main__":
+    main()
