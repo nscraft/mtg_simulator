efeffb7de7776b8b9af4316171ada57a7e7235e9
nick.craft <nscraft@gmail.com>
2024-02-14 09:30:37 -0700

    separating deck load deck gen

--- Project/Data/deck_loader.py
+++ Project/Data/deck_loader.py
@@ -5,6 +5,7 @@ import os
 class DeckExcelMethod:
     def __init__(self, file_name):
         self.deck_name = file_name
+        self.error_msg = 0
 
     def get_deck_name(self):
         return str(self.deck_name)
@@ -12,5 +13,7 @@ class DeckExcelMethod:
     def load_deck_excel(self):
         current_dir = os.path.dirname(__file__)
         excel_file_path = os.path.join(current_dir, self.deck_name)
-        return pd.read_excel(excel_file_path)
-
+        if not os.path.exists(excel_file_path):
+            self.error_msg = 1
+        else:
+            pd.read_excel(excel_file_path)
