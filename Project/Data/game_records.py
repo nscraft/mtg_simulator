080388f2b0763792f11b053701cb791acd85e2c0
Nick Craft <nscraft@gmail.com>
2024-02-14 19:58:19 -0700

    game wip

--- Project/Data/game_records.py
+++ Project/Data/game_records.py
@@ -4,23 +4,32 @@ import os
 
 def reset_save():
     records_library = pd.DataFrame()
-    records_library.to_csv("records_library.csv")
+    records_library.to_csv("records_library.csv", index=False)
     records_hand = pd.DataFrame()
-    records_hand.to_csv("records_hand.csv")
+    records_hand.to_csv("records_hand.csv", index=False)
     records_battlefield = pd.DataFrame()
-    records_battlefield.to_csv("records_battlefield.csv")
+    records_battlefield.to_csv("records_battlefield.csv", index=False)
 
 
 def update_records(game_library, game_hand, game_battlefield, game_turn):
-    records_library = pd.read_csv("records_library.csv")
+    try:
+        records_library = pd.read_csv("records_library.csv")
+    except pd.errors.EmptyDataError:
+        records_library = pd.DataFrame()
     game_library['turn'] = game_turn
-    pd.concat([records_library, game_library], ignore_index=True).to_csv("records_library.csv")
-    records_hand = pd.read_csv("records_hand.csv")
+    pd.concat([records_library, game_library], ignore_index=True).to_csv("records_library.csv", index=False)
+    try:
+        records_hand = pd.read_csv("records_hand.csv")
+    except pd.errors.EmptyDataError:
+        records_hand = pd.DataFrame()
     game_hand['turn'] = game_turn
-    pd.concat([records_hand, game_hand], ignore_index=True).to_csv("records_hand.csv")
-    records_battlefield = pd.read_csv("records_battlefield.csv")
+    pd.concat([records_hand, game_hand], ignore_index=True).to_csv("records_hand.csv", index=False)
+    try:
+        records_battlefield = pd.read_csv("records_battlefield.csv")
+    except pd.errors.EmptyDataError:
+        records_battlefield = pd.DataFrame()
     game_battlefield['turn'] = game_turn
-    pd.concat([records_battlefield, game_battlefield], ignore_index=True).to_csv("records_battlefield.csv")
+    pd.concat([records_battlefield, game_battlefield], ignore_index=True).to_csv("records_battlefield.csv", index=False)
 
 
 def destroy_files():
