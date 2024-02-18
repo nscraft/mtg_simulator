98715f879eba42b9edede71aaee8bebcbfc66dfa
Nick Craft <nscraft@gmail.com>
2024-02-18 07:47:36 -0700

    game wip

--- Project/Events/game_handler_improved.py
+++ Project/Events/game_handler_improved.py
@@ -5,19 +5,19 @@ import Project.Data.game_records_improved
 def run_game(deck_df, game_num):
     Project.Data.game_records_improved.destroy_files()
     Project.Data.game_records_improved.reset_gamestate_inMemory()
-    game = 1
+    game_loop = 0
     instance = Project.Events.game_improved.GameComponents(deck_df)
-    instance.set_commander_to_command_zone()
-    instance.set_deck_to_library()
-    while game <= game_num:
-        game += 1
+    while game_loop < game_num:
+        game_loop += 1
         instance.table.loc[:, 'game'] += 1
-        instance.shuffle()
-        turn = 0
+        turn_loop = 0
         instance.table.loc[:, 'turn'] = 0
+        instance.set_commander_to_command_zone()
+        instance.set_deck_to_library()
+        instance.shuffle()
         instance.draw_cards(7)
-        while turn < 10:
-            turn += 1
+        while turn_loop < 10:
+            turn_loop += 1
             instance.table.loc[:, 'turn'] += 1
             instance.draw_cards(1)
             instance.play_land()
--- Test_Project/Junk/scratch.py
+++ Test_Project/Junk/scratch.py
@@ -1,10 +1,9 @@
 import Project.Data.deck_gen
 import Project.Events.game_handler_improved
 import Project.Analytics.game_reporter_improved
-from Events.game_improved import GameComponents
 
 test_deck = Project.Data.deck_gen.gen_rand_deck()
 
-# result = Project.Events.game_handler_improved.run_game(test_deck, game_num=10)
+result = Project.Events.game_handler_improved.run_game(test_deck, game_num=3)
 
-#report = Project.Analytics.game_reporter_improved.game_report_multi()
+report = Project.Analytics.game_reporter_improved.game_report_multi()
