261acdfddd449c1f752c45bccb4f3d098e2dd973
Nick Craft <nscraft@gmail.com>
2024-02-18 08:12:17 -0700

    game wip

--- Test_Project/Junk/scratch.py
+++ Test_Project/Junk/scratch.py
@@ -4,6 +4,6 @@ import Project.Analytics.game_reporter_improved
 
 test_deck = Project.Data.deck_gen.gen_rand_deck()
 
-result = Project.Events.game_handler_improved.run_game(test_deck, game_num=3)
+result = Project.Events.game_handler_improved.run_game(test_deck, game_num=2)
 
 report = Project.Analytics.game_reporter_improved.game_report_multi()
