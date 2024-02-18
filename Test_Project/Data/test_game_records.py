4dbdd45159576d938d743c5b4d6239c2573b8fc1
Nick Craft <nscraft@gmail.com>
2024-02-18 09:00:38 -0700

    game wip

--- Test_Project/Data/test_game_records.py
+++ Test_Project/Data/test_game_records.py
@@ -1,18 +1,15 @@
 import unittest
 import pandas as pd
-from Project.Data.game_records import update_records, reset_records_inMemory, destroy_files
+import Project.Data.game_records
 
 
 class TestWriteDeck(unittest.TestCase):
 
     def setUp(self):
-        self.lib_df = pd.DataFrame({'column1': [1, 2, 3]})
-        self.ha_df = pd.DataFrame({'column1': [1, 2, 3]})
-        self.bf_df = pd.DataFrame({'column1': [1, 2, 3]})
+        self.gs_df = pd.DataFrame({'column1': [1, 2, 3]})
 
     def test_update_records(self):
-        update_records(game_library=self.lib_df, game_hand=self.ha_df, game_battlefield=self.bf_df, game_turn=1, game_num=1)
-        update_records(game_library=self.lib_df, game_hand=self.ha_df, game_battlefield=self.bf_df, game_turn=2, game_num=1)
+        Project.Data.game_records.update_gamestate_records(gamestate=self.gs_df)
 
 
 if __name__ == '__main__':
